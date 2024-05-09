f14e6e3c9ba80be5024ae39d0e59aee04d20b8ad
JUHEEBAEK <bjh9287@naver.com>
2024-05-09 21:49:55 +0900

    Feat: Card 컴포넌트

--- src/pages/portfolio/Card.tsx
+++ src/pages/portfolio/Card.tsx
@@ -1,5 +1,8 @@
 
 import DChip from "@/components/custom/DChip";
+
+import ultimate from '@/assets/screenshot/ultimate.png';
+
 import "./Card.scss";
 
 interface Props {
@@ -11,10 +14,14 @@ export default function ProjectCard({mainSkills, skills}: Props) {
   return (
     <article className="card">
       <header>
-        <h2>프로젝트 이름</h2>
-        <p>2023.05 ~ 2023.09</p>
+        <h2>OHCOACH Ultimate</h2>
+        <p>2020.01 ~ 2022.11</p>
       </header>
       <div className='card__content'>
+        <img src={ultimate} alt="OHCOACH Ultimate 화면" height="100" content=""  />
+        <section>
+          타사 서비스를 대체하기 위해 회사에 맞춤형으로 개발된 사내 시스템 구축. 구매, 근태, 생산, 출고 등 다양한 모듈을 포함하고 있는 서비스.
+        </section>
         <section>
           {mainSkills.map((select: SelectProperty) => (
             <DChip
@@ -37,6 +44,8 @@ export default function ProjectCard({mainSkills, skills}: Props) {
         </section>
         <section>
           <h3>성과</h3>
+          타사 서비스를 대체하기 위해 회사에 맞춤형으로 개발된 사내 시스템 구축.
+구매, 근태, 생산, 출고 등 다양한 모듈을 포함하고 있는 서비스.
         </section>
         <section>
           <h3>트러블슈팅</h3>
