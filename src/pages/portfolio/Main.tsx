ddaec3a872af97a840ca9581c2b920dc65fbdd00
JUHEEBAEK <bjh9287@naver.com>
2024-05-10 18:02:24 +0900

    Feat: Main loading 중 표시 형식

--- src/pages/portfolio/Main.tsx
+++ src/pages/portfolio/Main.tsx
@@ -1,6 +1,17 @@
-export default function Main() {
+import { useState } from 'react';
+
+import PROJECT_DATA from '@/data/DB_project.json';
+import LiveSection from '@/components/LiveSection';
+
+export default function MainPage() {
+  const [loading, setLoading] = useState(true);
+  const DB_PROJECT_DATAS = PROJECT_DATA as ProjectProperties[];
 
   return (
-    <h1>포폴페이지</h1>
+    <>
+      <h1>Projects</h1>
+      <h2>지금까지 참여한 프로젝트 리스트</h2>
+      { loading ? <LiveSection message='Please wait. Loading..' /> : null }
+    </>
   );
 }
