5c6725c9faaa7d769f9d09eda1bff96783a4f32e
JUHEEBAEK <bjh9287@naver.com>
2024-05-10 18:01:35 +0900

    Fix: 카드 컴포넌트에 props 형식으로 수정

--- src/pages/portfolio/Card.tsx
+++ src/pages/portfolio/Card.tsx
@@ -6,49 +6,50 @@ import ultimate from '@/assets/screenshot/ultimate.png';
 import "./Card.scss";
 
 interface Props {
+  name: string;
+  period: string;
+  description: string;
   mainSkills: any;
   skills: any;
+  keywords: any;
 }
-export default function ProjectCard({mainSkills, skills}: Props) {
+export default function ProjectCard({name, period, description, mainSkills, skills, keywords}: Props) {
 
   return (
     <article className="card">
       <header>
-        <h3>OHCOACH Ultimate</h3>
-        <p>2020.01 ~ 2022.11</p>
+        <h3>{name}</h3>
+        <p>{period}</p>
       </header>
       <div className='card__content'>
         <img src={ultimate} alt="OHCOACH Ultimate 화면" height="100" content=""  />
         <section>
-          타사 서비스를 대체하기 위해 회사에 맞춤형으로 개발된 사내 시스템 구축. 구매, 근태, 생산, 출고 등 다양한 모듈을 포함하고 있는 서비스.
+          <h4>프로젝트 소개</h4>
+          <p>{description}</p>
         </section>
         <section>
-          {mainSkills.map((select: SelectProperty) => (
-            <DChip
-              key={`mainSkill_${select.name}_${select.id}`}
-              size='small'
-              color={select.color}
-              label={select.name}
-              clickable={false}
-            />
-          ))}
-          {skills.map((select: SelectProperty, index: number) => (
-            <DChip
-            key={`skill_${select.name}_${index}`}
-            size='small'
-            color='grey'
-            label={select.name}
-            clickable={false}
-            />
-          ))}
+          <h4>Stack ICON</h4>
+          {/* TODO: stack Icon list로 변경하기 */}
+          <ul className='chipList'>
+            <li></li>
+          </ul>
         </section>
         <section>
-          <h3>성과</h3>
-          타사 서비스를 대체하기 위해 회사에 맞춤형으로 개발된 사내 시스템 구축.
-구매, 근태, 생산, 출고 등 다양한 모듈을 포함하고 있는 서비스.
+          <h4>키워드</h4>
+          <ul className='chipList'>
+            {keywords.map((select: SelectProperty, index: number) => (
+              <li><DChip
+                key={`skill_${select.name}_${index}`}
+                size='small'
+                color='grey'
+                label={select.name}
+                clickable={false}
+              /></li>
+            ))}
+          </ul>
         </section>
         <section>
-          <h3>트러블슈팅</h3>
+          <h4>상황</h4>
         </section>
       </div>
     </article>
