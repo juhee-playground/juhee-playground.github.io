09a0255da28dbf6eca347660c7e466c4a6d5aca9
JUHEEBAEK <bjh9287@naver.com>
2024-05-12 22:32:49 +0900

    Fix: point 요약 수정

--- src/constants/StackContent.tsx
+++ src/constants/StackContent.tsx
@@ -1,21 +1,25 @@
 const CONTENT_MAIN = [
   {
-    content: '웹 프론트엔드 개발 실무 경력(Vue)이 3년 이상인 사람',
+    content: '웹 프론트엔드 개발 실무 경력(Vue)이 3년 이상',
     link: 'https://dino100.notion.site/OHCOACH-Ultimate-fd838cf131fc4d718d2b4d89e7d42dd8',
     bold: '개발 실무 경력(Vue)이 3년',
   },
   {
-    content: '웹 프론트엔드의 기본적인 지식(Javascript)을 중요하게 생각하는 사람',
+    content: '웹 프론트엔드의 기본적인 지식(Javascript)을 중요하게 생각하고 활용 가능',
     link: 'https://github.com/juhee-playground/reading/blob/main/ModernJavascriptDeepDive/%EB%AA%A9%EC%B0%A8.md',
     bold: '(Javascript)',
   },
   {
-    content: 'TypeScript - React.js 이용한 토이 프로젝트 경험 보유한 사람',
+    content: '토이 프로젝트와 과제를 통해 작은 단위의 테스트 코드를 작성하고, React Testing Library와 Storybook을 활용',
+    bold: 'React Testing Library와 Storybook',
+  },
+  {
+    content: 'TypeScript - React.js 이용한 토이 프로젝트 경험 보유',
     link: 'https://dino100.notion.site/135e25adcbe64a1da56c5510688fd50c',
     bold: 'TypeScript - React.js',
   },
   {
-    content: '팀 플레이 및 소통을 중요시 여기는 사람',
+    content: '팀 플레이 및 소통을 중요하게 생각하고 행동',
     bold: '팀 플레이 및 소통',
   },
 ];
