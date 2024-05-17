a2a0b148c0f93212c1d60723a0c140ee4b7c58ab
JUHEEBAEK <bjh9287@naver.com>
2024-05-17 23:20:55 +0900

    Feat: Card: icon 추가, bordertype css 변경

--- src/pages/portfolio/Card.stories.tsx
+++ src/pages/portfolio/Card.stories.tsx
@@ -22,9 +22,10 @@ export const Playground: Story = {
   args: {
     name: 'OHCOACH Ultimate',
     period: '2020.01 ~ 2022.11',
+    borderType: 'typescript',
     description: '축구 데이터를 시각화하여 데이터를 효과적으로 전달하여 업무 효율을 높이는 서비스.',
-    // mainSkills: [{ name: 'vue', color: 'green', }, { name: 'javascript', color: 'yellow'}],
-    // skills: [{ name: 'd3' }, { name: 'highcharts.js'}, {name: 'Vuex'}],
-    keywords: [{ name: '데이터시각화' }, { name: 'd3'}, {name: 'Chart'}, {name: '기획팀 역제안'}, {name: 'pdf 기능'}, {name: '리펙토링'} ],
+    mainSkill: ['React','Typescript'],
+    skill: [{name: 'mysql', color: 'red', id: '1'}, {name: 'D3', color: 'orange', id: '2'} ],
+    keywords: [{ name: '데이터시각화', color: 'grey', id: '1' }, { name: 'd3', color: 'grey', id: '2'}, {name: 'Chart', color: 'grey', id: '3'}, {name: '기획팀 역제안', color: 'grey', id: '4'}, {name: 'pdf 기능', color: 'grey', id: '5'}, {name: '리펙토링', color: 'grey', id: '6'} ],
   },
 };
