ab525875576e9bdedc1fb65ee1e5df33e9d28ae8
JUHEEBAEK <bjh9287@naver.com>
2024-06-03 17:52:01 +0900

    Fix: 내용 수정 추후에 한번 더 수정하기

--- src/constants/description.ts
+++ src/constants/description.ts
@@ -1,22 +1,12 @@
 const CONTENT_MAIN = [
   {
-    content: '웹 프론트엔드 개발 실무 경력(Vue)이 3년 이상',
+    content: '웹 프론트엔드 개발 실무 경력 3년 이상',
     link: 'https://dino100.notion.site/OHCOACH-Ultimate-fd838cf131fc4d718d2b4d89e7d42dd8',
-    bold: '개발 실무 경력(Vue)이 3년',
-  },
-  {
-    content: '웹 프론트엔드의 기본적인 지식(Javascript)을 중요하게 생각하고 활용 가능',
-    link: 'https://github.com/juhee-playground/reading/blob/main/ModernJavascriptDeepDive/%EB%AA%A9%EC%B0%A8.md',
-    bold: '(Javascript)',
+    bold: '개발 실무 경력 3년',
   },
   {
     content: 'React Testing Library와 Storybook을 활용하여 코드 작성',
     bold: 'React Testing Library와 Storybook',
-  },
-  {
-    content: 'TypeScript - React.js 이용한 토이 프로젝트 경험 보유',
-    link: 'https://dino100.notion.site/135e25adcbe64a1da56c5510688fd50c',
-    bold: 'TypeScript - React.js',
   },
   {
     content: '팀 플레이 및 소통을 중요하게 생각하고 행동',
