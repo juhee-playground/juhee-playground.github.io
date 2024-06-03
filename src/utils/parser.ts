d42dfb6435b24f95adf2dc290fbfc0271d04a4d1
JUHEEBAEK <bjh9287@naver.com>
2024-06-03 17:44:36 +0900

    Style: fixme 지우기

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,7 +1,6 @@
 import dayjs from 'dayjs';
 
-// FIXME: utils은 보통 함수들이 들어가 있기 때문에, 파스칼 케이스로 파일을 만들지 않습니다. 또한 tsx는 jsx 문법을 사용하여 export하는 파일의 확장자이기 때문에, util은 .ts로 해도 됩니다.
-export function parserPeriod(date: PeriodDate): string {
+export function parserPeriod(date: IPeriodDate): string {
   const year = dayjs(date.end).diff(date.start, 'year');
   const month = dayjs(date.end).diff(date.start, 'month');
 
