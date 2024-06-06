1cce9f4c806e9266b751f4eddacc900bff918320
JUHEEBAEK <bjh9287@naver.com>
2024-06-06 19:57:34 +0900

    Chore: lint 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,15 +2,12 @@ module.exports = {
   root: true,
   env: { 
     browser: true,
-    es2020: true,
-    module: "node"
+    es2021: true,
   },
   extends: [
     "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
-    "plugin:import/recommended",
-    "plugin:jsx-a11y/recommended",
-    "eslint-config-prettier",
     "plugin:storybook/recommended"
   ],
   settings: {
@@ -28,8 +25,13 @@ module.exports = {
   },
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+  },
   plugins: ["react-refresh"],
   rules: {
+    "react/react-in-jsx-scope": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
