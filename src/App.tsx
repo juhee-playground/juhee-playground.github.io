be308581a22e9ca2a366a93ba0e3253dea2f9566
JUHEEBAEK <bjh9287@naver.com>
2024-06-06 20:01:43 +0900

    Style: Fixme 지우기

--- src/App.tsx
+++ src/App.tsx
@@ -23,8 +23,6 @@ const queryClient = new QueryClient({
   queryCache: new QueryCache({
     onError: (error, query) => {
       if ((error as AxiosError).code == 'ERR_NETWORK') {
-        // TODO: FIXME를 좀 더 업무 투로 적어주시면 좋을 것 같아요
-        //FIXME: 한번만 나오면 참 좋겠다~
         toast.error(`서버와 연결되지 않습니다`);
       }
       query.state.data;
