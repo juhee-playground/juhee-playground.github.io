8dfa918850272c13c6f5c61ba13787cd8cf34482
JUHEEBAEK <bjh9287@naver.com>
2024-06-06 22:31:27 +0900

    Fix: overview 내용 변경

--- src/constants/description.ts
+++ src/constants/description.ts
@@ -5,12 +5,16 @@ const CONTENT_MAIN = [
     bold: '개발 실무 경력 3년',
   },
   {
-    content: 'React Testing Library와 Storybook을 활용하여 코드 작성',
-    bold: 'React Testing Library와 Storybook',
-  },
-  {
     content: '팀 플레이 및 소통을 중요하게 생각하고 행동',
     bold: '팀 플레이 및 소통',
+  },
+  {
+    content: 'jest와 Storybook을 활용하여 단위테스트 코드 작성',
+    bold: 'jest와 Storybook',
+  },
+  {
+    content: '1명에서 10명 이상의 개발팀으로 성장하기까지의 초기 스타트업 경험 ',
+    bold: '초기 스타트업 경험',
   },
 ];
 export default CONTENT_MAIN;
