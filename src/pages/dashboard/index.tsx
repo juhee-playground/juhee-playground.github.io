b29e5e523c5534d5239cdb1bf1c8cf851509b6a4
JUHEEBAEK <bjh9287@naver.com>
2024-10-12 01:04:23 +0900

    Design: dashboard layout 수정

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,11 +6,20 @@ import './Main.scss';
 
 export default function MainPage() {
   return (
-    <article className='card__wrapper'>
-      <h1>Dashboard</h1>
-      <div className='card__container'>
-        <Card image={ReactIcon} name='React' count={4} />
-        <Card image={VueIcon} name='Vue' count={6} />
+    <article className='dashbaord__wrapper'>
+      <div className='widget__container'>
+        <h3 className='widget__title'>PROJECTS</h3>
+        <div className='widget__wrapper'>
+          <Card image={ReactIcon} name='React' count={4} />
+          <Card image={VueIcon} name='Vue' count={6} />
+        </div>
+      </div>
+      <div className='widget__container'>
+        <h3 className='widget__title'>COMPANY</h3>
+        <div className='widget__wrapper'>
+          <Card name='Fitogether' count={4} />
+          <Card name='YU partners' count={1} />
+        </div>
       </div>
     </article>
   );
