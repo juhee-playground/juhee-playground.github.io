299f870fd1385029e0d6168e26c5af26c6f9d688
JUHEEBAEK <bjh9287@naver.com>
2024-10-15 19:29:30 +0900

    Design: layout 수정

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,5 @@
 import ReactIcon from '@/assets/icon/React.svg';
 import VueIcon from '@/assets/icon/Vue.svg';
-import Card from '@/components/common/Card';
 import CardV2 from '@/components/common/CardVersion2';
 
 import './Main.scss';
@@ -8,13 +7,6 @@ import './Main.scss';
 export default function MainPage() {
   return (
     <article className='dashbaord__wrapper'>
-      <div className='widget__container'>
-        <h3 className='widget__title'>PROJECTS</h3>
-        <div className='widget__wrapper'>
-          <Card image={ReactIcon} name='React' count={4} />
-          <Card image={VueIcon} name='Vue' count={6} />
-        </div>
-      </div>
       <div className='widget'>
         <h3 className='title'>PROJECTS</h3>
         <div className='wrapper'>
@@ -25,8 +17,8 @@ export default function MainPage() {
       <div className='widget'>
         <h3 className='title'>COMPANY</h3>
         <div className='wrapper'>
-          <CardV2 name='Fitogether' count={4} />
-          <CardV2 name='YU partners' count={1} />
+          <CardV2 name='Fitogether' count={4} unit='year' />
+          <CardV2 name='YU partners' count={1} unit='year' />
         </div>
       </div>
     </article>
