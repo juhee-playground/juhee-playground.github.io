407c4e4d553aa7276efa69c9fead34b9a30645d3
JUHEEBAEK <bjh9287@naver.com>
2024-10-15 19:29:14 +0900

    Design: 크기, 패딩 등등 맞추기

--- src/components/common/CardVersion2.tsx
+++ src/components/common/CardVersion2.tsx
@@ -4,23 +4,25 @@ interface ICardProps {
   image?: string;
   name: string;
   count: number;
+  unit?: string;
 }
 
-const CardV2 = ({ image, name, count }: ICardProps) => {
+const CardV2 = ({ image, name, count, unit }: ICardProps) => {
   return (
-    <div className='card'>
+    <div className='card2'>
       {image ? (
-        <div className='card__image'>
+        <div className='card2__image'>
           <img src={image} alt={name} width={40} height={40} />
         </div>
       ) : (
-        <div className='card__name'>
+        <div className='card2__name'>
           <h4>{name}</h4>
         </div>
       )}
 
-      <div className='card__content'>
+      <div className='card2__content'>
         <h3>{count}</h3>
+        <p>{unit}</p>
       </div>
     </div>
   );
