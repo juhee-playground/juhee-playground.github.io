ab474b0b05fbd8684ba44f8d7770f28282d4df05
JUHEEBAEK <bjh9287@naver.com>
2024-10-22 22:46:59 +0900

    Style: 시맨틱 tag로 변경

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -11,45 +11,55 @@ import './Main.scss';
 export default function MainPage() {
   return (
     <div className='dashboard'>
-      <article className='dashboard__row'>
-        <div className='dashboard__widget'>
+      <section className='dashboard__row'>
+        <article className='dashboard__widget'>
           <h3 className='dashboard__widget-title'>PROJECTS</h3>
-          <div className='dashboard__widget-content'>
-            <CardV2 image={ReactIcon} name='React' count={3} />
-            <CardV2 image={VueIcon} name='Vue' count={5} />
-            <CardV2 image={PhpIcon} name='Php' count={2} />
-          </div>
-        </div>
-        <div className='dashboard__widget'>
-          <h3 className='dashboard__widget-title'>COMPANY</h3>
-          <div className='dashboard__widget-content'>
-            <CardV2 name='Fitogether' count={4} unit='year' />
-            <CardV2 name='YU partners' count={1} unit='year' />
-          </div>
-        </div>
-      </article>
-      <article className='dashboard__row'>
-        <div className='dashboard__widget'>
-          <h3 className='dashboard__widget-title'>STACK USAGE</h3>
+          <ul className='dashboard__widget-content'>
+            <li>
+              <CardV2 image={ReactIcon} name='React' count={3} />
+            </li>
+            <li>
+              <CardV2 image={VueIcon} name='Vue' count={5} />
+            </li>
+            <li>
+              <CardV2 image={PhpIcon} name='Php' count={2} />
+            </li>
+          </ul>
+        </article>
+        <article className='dashboard__widget'>
+          <h3 className='dashboard__widget-title'>COMPANYS</h3>
+          <ul className='dashboard__widget-content'>
+            <li>
+              <CardV2 name='Fitogether' count={4} unit='year' />
+            </li>
+            <li>
+              <CardV2 name='YU partners' count={1} unit='year' />
+            </li>
+          </ul>
+        </article>
+      </section>
+      <section className='dashboard__row'>
+        <article className='dashboard__widget'>
+          <h3 className='dashboard__widget-title'>MAIN TECH USAGE</h3>
           <div className='dashboard__widget-content'>
             <MyPieChart />
           </div>
-        </div>
+        </article>
 
-        {/* <div className='dashboard__widget'>
+        {/* <article className='dashboard__widget'>
           <h3 className='dashboard__widget-title'>경력그래프</h3>
           <div className='dashboard__widget-content'>
             <MyStackedBarChart />
           </div>
-        </div>
+        </article>
 
-        <div className='dashboard__widget'>
+        <article className='dashboard__widget'>
           <h3 className='dashboard__widget-title'>라인차트</h3>
           <div className='dashboard__widget-content'>
             <MyLineAreaChart />
           </div>
-        </div> */}
-      </article>
+        </article> */}
+      </section>
     </div>
   );
 }
