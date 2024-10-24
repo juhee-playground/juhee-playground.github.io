65b9603dfc5674e5efb4c6018469836a6f7d9f0c
JUHEEBAEK <bjh9287@naver.com>
2024-10-24 17:23:05 +0900

    Feat: bar chart 에 tooltip 기능 추가 - mouseEnter, mouseMove, mouseLeave

--- src/components/common/chart/Bars.tsx
+++ src/components/common/chart/Bars.tsx
@@ -6,9 +6,12 @@ interface IBarProps {
   xScale: d3.ScaleTime<number, number>;
   yPosition: number;
   barHeight: number;
+  onMouseEnter: (event: React.MouseEvent, task: ITask, color: string) => void;
+  onMouseMove: (event: React.MouseEvent) => void;
+  onMouseLeave: () => void;
 }
 
-const Bars = ({ tasks, xScale, yPosition, barHeight }: IBarProps) => {
+const Bars = ({ tasks, xScale, yPosition, barHeight, onMouseEnter, onMouseMove, onMouseLeave }: IBarProps) => {
   const barRadius = 5;
   const colors = ['hsl(235.93, 35%, 70%)', 'hsl(153.5, 40%, 70%)', 'hsl(188.98, 60%, 70%)'];
 
@@ -25,6 +28,9 @@ const Bars = ({ tasks, xScale, yPosition, barHeight }: IBarProps) => {
           rx={barRadius}
           ry={barRadius}
           fill={colors[i % tasks.length]}
+          onMouseEnter={event => onMouseEnter(event, task, colors[i % tasks.length])}
+          onMouseMove={onMouseMove}
+          onMouseLeave={onMouseLeave}
         />
       ))}
     </>
