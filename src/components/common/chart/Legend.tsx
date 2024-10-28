7b9aa020c309b0d3f7f835ad46a91bf7cb39fad6
JUHEEBAEK <bjh9287@naver.com>
2024-10-28 20:12:45 +0900

    Design: legend 스타일 맞추기

--- src/components/common/chart/Legend.tsx
+++ src/components/common/chart/Legend.tsx
@@ -1,23 +1,17 @@
+import './Legend.scss';
+
 interface ILegendProps {
   data: { id: string; label: string; value: number; color: string }[];
+  orientation?: 'horizontal' | 'vertical';
 }
 
-const Legend = ({ data }: ILegendProps) => {
+const Legend = ({ data, orientation = 'horizontal' }: ILegendProps) => {
   return (
-    <ul>
+    <ul className={`legend legend--${orientation}`}>
       {data.map(item => (
         <li key={item.id}>
-          <span
-            style={{
-              display: 'inline-block',
-              width: '12px',
-              height: '12px',
-              borderRadius: '50%',
-              backgroundColor: item.color,
-              marginRight: '8px',
-            }}
-          ></span>
-          <span>{item.label}</span>
+          <span className='color-indicator' style={{ backgroundColor: item.color }}></span>
+          <span className='label-text'>{item.label}</span>
         </li>
       ))}
     </ul>
