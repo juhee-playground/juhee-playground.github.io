5d73cc071bf41e27339624c7cd12b3360e173cd1
JUHEEBAEK <bjh9287@naver.com>
2024-10-29 22:30:59 +0900

    Fix: Header에 설정도 같이 넣기

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,13 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import ClearIcon from '@mui/icons-material/Clear';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import PrintIcon from '@mui/icons-material/Print';
+import SettingsIcon from '@mui/icons-material/Settings';
+
 import IconButton from '@mui/material/IconButton';
 import { useTheme } from '@mui/material/styles';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 
 import { ColorModeContext } from '@/context/ColorModeContext';
+import ThemeCustomized from '@/layout/ThemeCustomized';
 import { useAppSelector, useAppDispatch } from '@/redux/hooks';
 import { changePrintMode } from '@/redux/modules/settings';
 import type { TRootState } from '@/redux/store';
@@ -16,9 +20,29 @@ import './Header.scss';
 
 export default function DenseAppBar() {
   const dispatch = useAppDispatch();
+  const [menuDirection, setMenuDirection] = useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
   const theme = useTheme();
   const colorMode = React.useContext(ColorModeContext);
-  const { isPrintMode } = useAppSelector((state: TRootState) => state.settings);
+  const { pointColor, isPrintMode } = useAppSelector((state: TRootState) => state.settings);
+  const anchor = 'right';
+  const mode = isPrintMode ? 'print' : '';
+
+  const toggleDrawer = (direction: TAnchor, open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+
+    setMenuDirection({ ...menuDirection, [direction]: open });
+  };
 
   const handleClickOpen = async () => {
     // change print mode
@@ -77,6 +101,22 @@ export default function DenseAppBar() {
           </>
         )}
       </div>
+      <button
+        style={{ backgroundColor: pointColor.hex }}
+        className={`fixButton half-left toggler ripple ${isPrintMode ? `fixButton--${mode}` : ''}`}
+        onClick={toggleDrawer(anchor, true)}
+      >
+        <SettingsIcon />
+      </button>
+
+      <SwipeableDrawer
+        anchor={anchor}
+        open={menuDirection[anchor]}
+        onClose={toggleDrawer(anchor, false)}
+        onOpen={toggleDrawer(anchor, true)}
+      >
+        <ThemeCustomized />
+      </SwipeableDrawer>
     </header>
   );
 }
